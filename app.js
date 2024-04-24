3687a0d779e47d924a0d398f6fe9677c77724419
mishalalshahari <mishalalshahari@gmail.com>
2024-04-25 01:43:59 +0530

    understanding jsx pt2

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => ReactElement - JS Object => HTMLElement(render)
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello, React!"
-);
-console.log(heading);
-
 //JSX - HTML-like or XML-like syntax extension for ECMAScript without any defined semantics.
 //js engine doesn't understand jsx but parcel is doing it for us
 //jsx (transpiled before it reaches the js engine by parcel(acting as manager) with the help of Babel)
-//JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
-const jsxHeading = <h1 id="heading">Hello, React!</h1>;
+//JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement(render)
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">Hello, React!</h1>
+); //mandatory to use brackets for multiple lines of jsx inorder to make the compiler understand where the jsx starts and ends
+
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
