badff7c8321e0ea55ef420584f2662aedfdd3c54
mishalalshahari <mishalalshahari@gmail.com>
2024-04-26 02:06:16 +0530

    component composition

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = (
-  <h1 className="head" tabIndex="1">
+const Title = () => (
+  <h1 className="head" tabIndex="5">
     Hello, React!
   </h1>
 );
 
-// React Functional Component
-// A react functional component is a plain JavaScript function that accepts props as an argument and returns a React element.
-const HeadingComponent = () => {
-  return <h1>Hello, React Functional Component!</h1>;
-}
-const HeadingComponent2 = () => <h1>Hello, React Functional Component!</h1>;
-//HeadingComponent and HeadingComponent2 are same
-
-// Class Based Component - OLD
-// Functional Component - NEW
+//component composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">
+      React Functional Component
+    </h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
